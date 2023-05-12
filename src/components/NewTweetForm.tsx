47f130ed97b018d59c3dca7e53009338400928ed
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-11 22:33:18 -0500

    working on new tweet form, added button component and button props. added prifile image, pulled from discord.

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,19 @@
+import { useSession } from "next-auth/react"
+import { Button } from "./Button"
+import { ProfileImage } from "./ProfileImage"
+
+export function NewTweetForm() {
+    const session = useSession();
+
+    if (session.status !== "authenticated") return;
+    
+    return (
+        <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
+            <div className="flex gap-4">
+              { <ProfileImage src={session.data.user.image} /> }
+                <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="What's happening?"/>
+                <Button className="self-end"></Button>
+            </div>
+        </form>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/utils/api";
+import { SideNav } from "~/components/SideNav";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +13,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Head>
+        <title>Social Media</title>
+        <meta name="description" content="It's 2023, you know what social media is." />
+      </Head>
+      <div className="container mx-auto flex sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
     </SessionProvider>
   );
 };
