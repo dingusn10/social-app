40f6e294ddb9d96f385a1da2074f1bd77a1b1eb6
dingusn10 <87838062+dingusn10@users.noreply.github.com>
2023-05-12 14:28:37 +0000

    new post form now fits input size. fixed build errors

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,17 +1,41 @@
 import { useSession } from "next-auth/react"
 import { Button } from "./Button"
+import { useLayoutEffect, useState, useRef, useCallback } from "react"
 import { ProfileImage } from "./ProfileImage"
 
-export function NewTweetForm() {
-    const session = useSession();
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+    if (textArea == null) return
+    textArea.style.height = "0"
+    textArea.style.height = `${textArea.scrollHeight}px`
+}
 
-    if (session.status !== "authenticated") return;
+export function NewTweetForm() {
+   const session = useSession();
+   if (session.status != "authenticated") return null;
+
+   return <Form />
+}
+
+function Form() {
+    const session = useSession();
+    const [inputValue, setInputValue] = useState("");
+    const textAreaRef = useRef<HTMLTextAreaElement>()
+    const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+        updateTextAreaSize(textArea)
+        textAreaRef.current = textArea
+    }, [])
+
+    useLayoutEffect(() => {
+        updateTextAreaSize(textAreaRef.current)
+    }, [inputValue])
+
+    if (session.status !== "authenticated") return null;
     
     return (
         <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
             <div className="flex gap-4">
               { <ProfileImage src={session.data.user.image} /> }
-                <textarea className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="What's happening?"/>
+                <textarea ref={inputRef} style={{height: 0}} value={inputValue} onChange={(e) => setInputValue(e.target.value)} className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" placeholder="What's happening?"/>
                 <Button className="self-end"></Button>
             </div>
         </form>
